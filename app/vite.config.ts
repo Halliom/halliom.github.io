a5632f96bcc59cf04755f7c30f44cd334abb83b9
Johan Wieslander <johan.jww@hotmail.com>
2026-01-10 16:18:34 +0000

    Move font

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tailwindcss()],
-})
+  base: "/",
+  plugins: [
+    react(),
+    tailwindcss(),
+    webfontDownload(
+      "https://fonts.googleapis.com/css2?family=Domine:wght@400..700&display=swap"
+    ),
+  ],
+});
